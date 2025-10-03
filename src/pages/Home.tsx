5a5b13e8c8207170629c378877ba6e8b5c07ac20
imlocle <loc.tan.le@gmail.com>
2025-10-03 07:15:06 -0700

    Home page adding ServicesSection, removing What I Do

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,15 @@
 import { motion } from 'framer-motion';
 import '../styles/Home.css';
-import { FaServer, FaAws, FaRobot, FaDatabase } from 'react-icons/fa';
-import { SiTerraform, SiAwslambda } from 'react-icons/si';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import raxLogo from '../assets/rax-logo-2020.svg';
 import neptuneLogo from '../assets/neptune-logo2.png';
 import mdsLogo from '../assets/mds.png';
 import wayviaLogo from '../assets/wayvia-logo.svg';
 import { POSITION_TITLE } from '../utils/constants';
-import GetInTouch from '../components/GetInTouch';
-import SkillCard from '../components/SkillCard';
 import CompanyCard from '../components/CompanyCard';
-// import Testimonials from '../components/Testimonials';
+import ServicesSection from '../components/section/ServicesSection';
+import ButtonLink from '../components/button/ButtonLink';
+import Cta from '../components/Cta';
 
 const Home = () => {
   const { pathname } = useLocation();
@@ -49,7 +47,7 @@ const Home = () => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 0.5, duration: 0.8 }}
           >
-            Building robust and scalable solutions
+            I build scalable AI-powered backend systems for startups.
           </motion.p>
 
           <motion.div
@@ -58,12 +56,16 @@ const Home = () => {
             animate={{ opacity: 1 }}
             transition={{ delay: 0.8, duration: 0.8 }}
           >
-            <Link to="/contact" className="primary-button">
-              Get In Touch
-            </Link>
-            <Link to="/work" className="secondary-button">
-              View My Work
-            </Link>
+            <ButtonLink
+              link="/contact"
+              text="🚀 Let's Build Together"
+              className="primary-button"
+            />
+            <ButtonLink
+              link="/work"
+              text="View My Work"
+              className="secondary-button"
+            />
           </motion.div>
         </motion.div>
 
@@ -79,7 +81,11 @@ const Home = () => {
         </motion.div>
       </div>
 
-      <motion.section
+      <ServicesSection />
+
+      {/* What I Do Section */}
+
+      {/* <motion.section
         className="skills-section"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -87,7 +93,6 @@ const Home = () => {
         viewport={{ once: true, amount: 0.3 }}
       >
         <h2 className="section-title">What I Do</h2>
-
         <div className="skills-grid">
           <SkillCard
             icon={FaAws}
@@ -120,9 +125,7 @@ const Home = () => {
             description="Building data pipelines and analytics systems for actionable insights"
           />
         </div>
-      </motion.section>
-
-      {/* <Testimonials /> */}
+      </motion.section> */}
 
       <motion.section
         className="companies-section"
@@ -154,7 +157,7 @@ const Home = () => {
             alt="Wayvia"
           />
         </div>
-        <GetInTouch />
+        <Cta />
       </motion.section>
     </div>
   );
