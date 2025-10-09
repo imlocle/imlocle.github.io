5d7e92cd91eaab78dfacc864a95761c04f13f7b9
imlocle <loc.tan.le@gmail.com>
2025-10-09 15:33:12 -0700

    footer description update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { GITHUB_LINK, LINKEDIN_LINK, POSITION_TITLE } from '../utils/constants';
+import { GITHUB_LINK, LINKEDIN_LINK } from '../utils/constants';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -12,7 +12,7 @@ const Footer = () => {
         <div className="footer-section">
           <h3 className="footer-title">Loc Le</h3>
           <p className="footer-description">
-            {POSITION_TITLE} looking to continuously learn, explore, and grow.
+            Building Tomorrow's Cloud Solutions Today. Expert Backend Engineering & Consulting.
           </p>
         </div>
 
