4cc23b5a6d43a257dccaff63d77a5ca17fb0390a
imlocle <loc.tan.le@gmail.com>
2025-10-10 13:49:35 -0700

    ServicesSectionCard

--- src/components/SkillCategory.tsx
+++ src/components/SkillCategory.tsx
@@ -9,9 +9,9 @@ const SkillCategory = ({
 }) => {
   return (
     <motion.div
-      whileHover={{ y: -10, boxShadow: '0 10px 30px rgba(0, 0, 0, 0.1)' }}
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
+      whileHover={{ y: -10, boxShadow: '0 10px 30px rgba(0, 0, 0, 0.1)' }}
       transition={{ duration: 0.5, delay: 0.2 }}
       viewport={{ once: true }}
     >
